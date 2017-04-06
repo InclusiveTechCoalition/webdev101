5f078d1c16f79c00235ab7ed08383439d32e0c0a
Alexa <alexa.anderson@outlook.com>
2017-04-06 02:47:40 -0500

    hide lessons until click

--- src/components/CoursePanel.js
+++ src/components/CoursePanel.js
@@ -2,18 +2,40 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class CoursePanel extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      unfoldedCourse: null
+    }
+    this.setUnfoldedCourse = this.setUnfoldedCourse.bind(this);
+    this.unfoldedCourseClass = this.unfoldedCourseClass.bind(this);
+  }
+
+  setUnfoldedCourse(courseTitle) {
+    this.setState({unfoldedCourse: courseTitle})
+  }
+
+  unfoldedCourseClass(courseTitle) {
+    return this.state.unfoldedCourse === courseTitle ? 'unfolded-course' : '';
+  }
   render() {
     const course = this.props.course;
 
     function lessonRoute(lesson) {
-      return `/webdev101/course/${course.id}/l/${lesson.route}`
+      return `/webdev101/course/${course.id}/l/${lesson.route}`;
     }
 
+
     return (
-      <div className="course-panel">
+      <div className={`course-panel ${this.unfoldedCourseClass(course.title)}`}>
         <Link to={`/webdev101/course/${course.id}/l/${course.lessons[0].route}`}
               className='course-title'>{course.title}
         </Link>
+        <span className="fa-stack clickable" onClick={() => this.setUnfoldedCourse(course.title)}>
+          <i className="fa fa-circle-thin fa-stack-2x" aria-hidden="true"></i>
+          <i className="fa fa-chevron-right fa-stack-1x" aria-hidden="true"></i>
+        </span>
         <ul className='lesson-list'>
           {course.lessons.map((lesson, idx) => {
             return(
